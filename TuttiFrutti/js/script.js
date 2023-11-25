ab55b88d21c480b63942bc24c6ab3383abac65c3
Anymoose98 <daniel.digiacomo98@gmail.com>
2023-11-25 17:20:12 +0100

    aggiunto pesca

--- TuttiFrutti/js/script.js
+++ TuttiFrutti/js/script.js
@@ -1,3 +1,7 @@
 // lista frigorifero
 let frigorifero = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola']
 console.log(frigorifero)
+
+// aggiungere pesca
+frigorifero.push("pesca")
+console.log(frigorifero)
