e1aa10b9c28f433b60dee15d41003c9591c7aaac
Anymoose98 <daniel.digiacomo98@gmail.com>
2023-11-25 17:32:27 +0100

    Esercizio array-lista fatto

--- TuttiFrutti/js/script.js
+++ TuttiFrutti/js/script.js
@@ -5,3 +5,20 @@ console.log(frigorifero)
 // aggiungere pesca
 frigorifero.push("pesca")
 console.log(frigorifero)
+
+
+// cercare cocomero
+let esito
+
+for(let i = 0; i < frigorifero.length; i++){
+    if("cocomero" == frigorifero[i]){
+        esito = true
+    }
+}
+
+    if(esito == true){
+        console.log("Trovato! Devo solo preparare il cocktail.")
+    }
+    else{
+        console.log("Oh no, devo uscire a comprare il cocomero!")
+    }
